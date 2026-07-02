32ae9e1f3876cb94134b20ed3060e09ec2060703
Pawan Kumar <99436894+pawankumar1214@users.noreply.github.com>
2026-07-02 14:06:21 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0500-keyboard-row/0500-keyboard-row.py
+++ 0500-keyboard-row/0500-keyboard-row.py
@@ -1,7 +1,7 @@
 class Solution:
     def findWords(self, words: List[str]) -> List[str]:
         keyboard = ("qwertyuiop", "asdfghjkl", "zxcvbnm")
-        out = []
+        res = []
 
         def isWordOnRow(word, row):
             for w in word.lower():
@@ -14,4 +14,4 @@ class Solution:
                 if isWordOnRow(word, row):
                     out.append(word)
                     break
-        return out
+        return res
