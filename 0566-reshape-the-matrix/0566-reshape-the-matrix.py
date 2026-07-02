bdaef6cdc62c354f1afc78d982987208745b95db
Pawan Kumar <99436894+pawankumar1214@users.noreply.github.com>
2026-07-02 16:02:10 +0530

    Time: 2 ms (35.08%), Space: 19.8 MB (90.21%) - LeetHub

---
+++ 0566-reshape-the-matrix/0566-reshape-the-matrix.py
@@ -0,0 +1,14 @@
+class Solution:
+    def matrixReshape(self, mat: List[List[int]], r: int, c: int) -> List[List[int]]:
+        m, n = len(mat), len(mat[0])
+        if m*n != r*c: return mat
+
+        reshaped = [[0] * c for _ in range(r)]
+        count = 0
+
+        for i in range(m):
+            for j in range(n):
+                reshaped[count // c][count % c] = mat[i][j]
+                count += 1
+        
+        return reshaped
