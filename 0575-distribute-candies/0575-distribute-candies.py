fc09427ef6c2e1b560491d27c1390321b758626e
Pawan Kumar <99436894+pawankumar1214@users.noreply.github.com>
2026-07-03 11:40:34 +0530

    Time: 24 ms (40.05%), Space: 21.3 MB (10.29%) - LeetHub

--- 0575-distribute-candies/0575-distribute-candies.py
+++ 0575-distribute-candies/0575-distribute-candies.py
@@ -1,6 +1,3 @@
 class Solution:
     def distributeCandies(self, candyType: List[int]) -> int:
-        n = len(candyType)//2
-        s = len(set(candyType))
-
-        return min(n,s)
+        return min(len(candyType)//2, len(set(candyType)))
